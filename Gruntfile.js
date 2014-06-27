7129e17b0b7d452e96a21aa710d5d0356f04fb8e
Masahiro Yamauchi <yamauchi@geishatokyo.com>
2014-06-27 21:27:57 +0900

    Add Gruntfile to compile and test

---
+++ Gruntfile.js
@@ -0,0 +1,30 @@
+module.exports = function(grunt) {
+    grunt.loadNpmTasks('grunt-typescript');
+    grunt.loadNpmTasks('grunt-mocha-phantomjs');
+    grunt.initConfig({
+        typescript: {
+            main: {
+                src: ['src/mml2miku.ts'],
+                dest: 'js/mml2miku.js',
+                options: {
+                    comments: true,
+                    // target: 'es5',
+                }
+            },
+            test: {
+                src: ['tests/testmml2miku.ts'],
+                // dest: 'tests/testmml2miku.js',
+                options: {
+                    module: 'commonjs',
+                    comments: true,
+                    // target: 'es5',
+                }
+            }
+        },
+        mocha_phantomjs: {
+            all: ['test.html']
+        }
+    });
+    grunt.registerTask('compile', ['typescript']);
+    grunt.registerTask('test', ['mocha_phantomjs']);
+};
